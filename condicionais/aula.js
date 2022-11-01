eb6e3b4cc477768f5461c777b692aa3180000a41
Alysson Sousa <alyssos@hotmail.com>
2022-11-01 16:32:06 -0300

    condicional

---
+++ condicionais/aula.js
@@ -0,0 +1,8 @@
+const numero = 10;
+//const eNumeroPar = true;
+//const eNumeroPar = numero % 2;
+const eNumeroPar = (numero % 2) == 0;
+console.log(`numero ${numero} é par?`);
+console.log("resposta:" + eNumeroPar);
+
+
