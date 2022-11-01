b22bcadd08dce74460ad7b99b091e5f607ed5d2a
Alysson <alyssos@hotmail.com>
2022-11-01 16:56:26 -0300

    condicional

--- condicionais/aula.js
+++ condicionais/aula.js
@@ -1,8 +1,21 @@
-const numero = 10;
 //const eNumeroPar = true;
 //const eNumeroPar = numero % 2;
-const eNumeroPar = (numero % 2) == 0;
+/*
+= atribui
+== compara sem tipo 
+const eNumeroPar = (numero % 2) === 0;
+=== compara com tipo 
+const eNumeroPar = (numero % 2) === "0";
+
+*/
+const numero = 0;
+const eNumeroPar = (numero % 2) === 0; 
 console.log(`numero ${numero} é par?`);
 console.log("resposta:" + eNumeroPar);
 
+if(numero === 0){
+    console.log('numero é invalido!');
+}else if(eNumeroPar){
+    console.log('numero é par :)');
+}else{console.log("Erro")}
 
