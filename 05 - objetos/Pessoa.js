6a514857e0434435089e0d251b12c61e622502ac
Alysson <alyssos@hotmail.com>
2022-11-04 16:35:12 -0300

    Pessoa

--- 05 - objetos/Pessoa.js
+++ 05 - objetos/Pessoa.js
@@ -1,14 +1,23 @@
 class Pesssoa {
     nome;
     idade;
+    anoDeNascimento;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2022 - idade;
+    }
 
     descrever() {
         console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
     }
-};
+}
 
 const alysson = new Pesssoa();
-alysson.nome = "Alysson";
-alysson.idade = "100";
+const alysson2 = new Pesssoa('alysson2',100);
 
+alysson.nome = "Alysson";
+alysson.idade = 100;
 alysson.descrever();
+console.log(alysson2);
