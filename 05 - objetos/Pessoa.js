dc2447f86b3883d770b6281b8dfaa0773d0c01ac
Alysson <alyssos@hotmail.com>
2022-11-04 17:38:12 -0300

    Pessoa

--- 05 - objetos/Pessoa.js
+++ 05 - objetos/Pessoa.js
@@ -15,9 +15,26 @@ class Pesssoa {
 }
 
 const alysson = new Pesssoa();
-const alysson2 = new Pesssoa('alysson2',100);
+const alysson2 = new Pesssoa('alysson2', 24);
 
 alysson.nome = "Alysson";
-alysson.idade = 100;
+alysson.idade = 24;
 alysson.descrever();
+
+console.log('\n');
+
 console.log(alysson2);
+
+console.log('\n');
+
+function compararPessoas(p1, p2) {
+    if (p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
+    } else if (p2.idade > p1.idade) {
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`);
+    }
+}
+
+compararPessoas(alysson, alysson2);
