8ab71b1497edfc6202a0b1b9185286bcd99d9d04
Alysson <alyssos@hotmail.com>
2022-11-04 18:10:57 -0300

    Pessoa

--- 05 - objetos/lista_exercicio_objeto02.js
+++ 05 - objetos/lista_exercicio_objeto02.js
@@ -16,12 +16,40 @@ class Pessoa {
     calcularImc() {
         return this.peso / (this.altura * this.altura);
     }
+    classificarImc() {
+        const imc = this.calcularImc();
+        if (imc < 18.5) {
+            return ` Abaixo de 18.5 Abaixo do peso: ${imc}`;
+        } else if (imc >= 18.5 && imc < 25) {
+            return ` Entre 25 e 30 acima do peso: ${imc}`;
+        } else if (imc >= 25 && imc <= 30) {
+            return ` Entre 25 e 30 acima do peso: ${imc}`;
+        } else if (imc >= 30 && imc <= 40) {
+            return ` Entre 30 e 40 Obeso: ${imc}`;
+        } else if (imc > 40) {
+            return ` Acima de 40 Obesidade grave: ${imc}`;
+        } else {
+            return ` fora do paramentro de comparação ERRO!: ${imc}`;
+        }
+    }
 
 }
+
 const jose = new Pessoa("jose", 70, 1.75);
 console.log(jose);
 console.log(jose.calcularImc());
+console.log(jose.classificarImc());
+console.log("\n");
 
 const renan = new Pessoa("renan", 63, 1.75);
 console.log(renan);
 console.log(renan.calcularImc());
+console.log(renan.classificarImc());
+console.log("\n");
+
+
+const vitor = new Pessoa("vitor", 60, 1.69);
+console.log(vitor);
+console.log(vitor.calcularImc());
+console.log(vitor.classificarImc());
+
