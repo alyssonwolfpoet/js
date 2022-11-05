e33220e9bb44bf28c93c7905679986e7ba01687d
Alysson <alyssos@hotmail.com>
2022-11-05 15:32:28 -0300

    listas

--- 06 - listas (arrays)/exercicio_array02.js
+++ 06 - listas (arrays)/exercicio_array02.js
@@ -1,2 +1,8 @@
 //2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado.
-const numeros = [1,2,3,4,5,6,7,8,9,0];
+const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i <= numeros.length; i++) {
+    if(numeros[i]%2 == 0){
+        console.log(numeros[i]);
+    }
+}
