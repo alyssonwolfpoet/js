b2680a74ea48715bb12bb7662e15f4b7e9adbb68
Alysson <alyssos@hotmail.com>
2022-11-05 15:34:09 -0300

    listas

--- 06 - listas (arrays)/exercicio_array06.js
+++ 06 - listas (arrays)/exercicio_array06.js
@@ -1,2 +1,2 @@
-//5) Dado uma lista com as médias tiradas pelos alunos. Imprima todas as médias que ficaram de recuperação (média < 5).
-//   Ex de lista = [2, 7, 3, 8, 10, 4]
+//6) Dado uma lista com as notas tiradas pelos alunos. imprima a maior nota.
+//   Ex de lista de notas = [2, 7, 3, 8, 10, 4]
