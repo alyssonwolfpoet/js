be21fa2ac6b88aa910292bdfbfb67a38d5cc94c3
Alysson <alyssos@hotmail.com>
2022-11-05 15:47:47 -0300

    listas

--- 06 - listas (arrays)/exercicio_array03.js
+++ 06 - listas (arrays)/exercicio_array03.js
@@ -1,1 +1,8 @@
 //3) Faça um programa que dado uma lista de nomes imprima os nomes que começem com a letra "V".
+let nomes = [];
+nomes.push("Alysson");
+nomes.push("vitor");
+
+array.forEach(element => {
+    
+})
